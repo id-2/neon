21dffc1b3bd9ecb98d460c2ebbad261937310e09
Joonas Koivunen <joonas@neon.tech>
2024-07-11 11:45:34 +0000

    feat(tad): idempotency on completion
    now historically detached timelines will on retry return 200 OK, with
    stable ordered reparented timelines. these timelines will be subject to
    future changes (like timeline archival, or having deleted some
    timelines).

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -4731,13 +4731,7 @@ impl Timeline {
         tenant: &crate::tenant::Tenant,
         options: detach_ancestor::Options,
         ctx: &RequestContext,
-    ) -> Result<
-        (
-            completion::Completion,
-            detach_ancestor::PreparedTimelineDetach,
-        ),
-        detach_ancestor::Error,
-    > {
+    ) -> Result<detach_ancestor::Progress, detach_ancestor::Error> {
         detach_ancestor::prepare(self, tenant, options, ctx).await
     }
 
