707ec2c2071b166f6496702215b87a679ec1bc36
Alex Chi Z <chi@neon.tech>
2024-07-11 16:43:21 -0400

    resolve comments and add tests
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- pageserver/src/tenant/timeline/compaction.rs
+++ pageserver/src/tenant/timeline/compaction.rs
@@ -43,15 +43,22 @@ use pageserver_compaction::interface::*;
 
 use super::CompactionError;
 
+/// Maximum number of deltas before generating an image layer in bottom-most compaction.
+const COMPACTION_DELTA_THRESHOLD: usize = 5;
+
 /// The result of bottom-most compaction for a single key at each LSN.
-struct KeyLogAtLsn(Vec<(Lsn, Value)>);
+#[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
+pub struct KeyLogAtLsn(pub Vec<(Lsn, Value)>);
 
 /// The result of bottom-most compaction.
-struct KeyHistoryRetention {
+#[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
+pub(crate) struct KeyHistoryRetention {
     /// Stores logs to reconstruct the value at the given LSN, that is to say, logs <= LSN or image == LSN.
-    below_horizon: Vec<(Lsn, KeyLogAtLsn)>,
+    pub(crate) below_horizon: Vec<(Lsn, KeyLogAtLsn)>,
     /// Stores logs to reconstruct the value at any LSN above the horizon, that is to say, log > LSN.
-    above_horizon: KeyLogAtLsn,
+    pub(crate) above_horizon: KeyLogAtLsn,
 }
 
 impl KeyHistoryRetention {
@@ -1022,7 +1029,7 @@ impl Timeline {
     /// above_horizon    -> deltas=[+F@0x60]             full history above the horizon
     ///
     /// Note that `accumulated_values` must be sorted by LSN and should belong to a single key.
-    async fn generate_key_retention(
+    pub(crate) async fn generate_key_retention(
         self: &Arc<Timeline>,
         key: Key,
         history: &[&(Key, Lsn, Value)],
@@ -1211,7 +1218,7 @@ impl Timeline {
             let layers = guard.layer_map();
             let gc_info = self.gc_info.read().unwrap();
             let mut retain_lsns_below_horizon = Vec::new();
-            let gc_cutoff = Lsn::min(gc_info.cutoffs.horizon, gc_info.cutoffs.pitr);
+            let gc_cutoff = gc_info.min_cutoff();
             for lsn in &gc_info.retain_lsns {
                 if lsn < &gc_cutoff {
                     retain_lsns_below_horizon.push(*lsn);
@@ -1361,7 +1368,7 @@ impl Timeline {
                         &accumulated_values,
                         gc_cutoff,
                         &retain_lsns_below_horizon,
-                        5,
+                        COMPACTION_DELTA_THRESHOLD,
                     )
                     .await?;
                 // Put the image into the image layer. Currently we have a single big layer for the compaction.
@@ -1393,7 +1400,7 @@ impl Timeline {
                 &accumulated_values,
                 gc_cutoff,
                 &retain_lsns_below_horizon,
-                5,
+                COMPACTION_DELTA_THRESHOLD,
             )
             .await?;
         // Put the image into the image layer. Currently we have a single big layer for the compaction.
