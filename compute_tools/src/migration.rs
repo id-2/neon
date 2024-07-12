d1ea1f01553daa11d2163684d936624842383b5f
Tristan Partin <tristan@neon.tech>
2024-07-12 13:19:27 -0500

    Bleh

--- compute_tools/src/migration.rs
+++ compute_tools/src/migration.rs
@@ -1,21 +1,51 @@
+use std::{collections::HashMap, fmt::Display};
+
 use anyhow::{Context, Result};
-use postgres::Client;
+use compute_api::spec::Database;
+use postgres::{Client, NoTls};
 use tracing::info;
+use url::Url;
+
+use crate::pg_helpers::get_existing_dbs;
+
+pub(crate) enum Migration<'m> {
+    Cluster(&'m str),
+    PerDatabase(&'m str),
+}
+
+impl<'m> Display for Migration<'m> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Cluster(migration) | Self::PerDatabase(migration) => f.write_str(migration),
+        }
+    }
+}
 
 pub(crate) struct MigrationRunner<'m> {
-    client: &'m mut Client,
-    migrations: &'m [&'m str],
+    connstr: Url,
+    cluster_client: Client,
+    migrations: &'m [Migration<'m>],
 }
 
 impl<'m> MigrationRunner<'m> {
-    pub fn new(client: &'m mut Client, migrations: &'m [&'m str]) -> Self {
-        Self { client, migrations }
+    pub fn new(mut connstr: Url, migrations: &'m [Migration<'m>]) -> Result<Self> {
+        connstr
+            .query_pairs_mut()
+            .append_pair("application_name", "migrations");
+
+        let cluster_client = Client::connect(connstr.as_str(), NoTls)?;
+
+        Ok(Self {
+            connstr,
+            cluster_client,
+            migrations,
+        })
     }
 
     fn get_migration_id(&mut self) -> Result<i64> {
         let query = "SELECT id FROM neon_migration.migration_id";
         let row = self
-            .client
+            .cluster_client
             .query_one(query, &[])
             .context("run_migrations get migration_id")?;
 
@@ -28,7 +58,7 @@ impl<'m> MigrationRunner<'m> {
             self.migrations.len()
         );
 
-        self.client
+        self.cluster_client
             .simple_query(&setval)
             .context("run_migrations update id")?;
 
@@ -37,21 +67,50 @@ impl<'m> MigrationRunner<'m> {
 
     fn prepare_migrations(&mut self) -> Result<()> {
         let query = "CREATE SCHEMA IF NOT EXISTS neon_migration";
-        self.client.simple_query(query)?;
+        self.cluster_client.simple_query(query)?;
 
         let query = "CREATE TABLE IF NOT EXISTS neon_migration.migration_id (key INT NOT NULL PRIMARY KEY, id bigint NOT NULL DEFAULT 0)";
-        self.client.simple_query(query)?;
+        self.cluster_client.simple_query(query)?;
 
         let query = "INSERT INTO neon_migration.migration_id VALUES (0, 0) ON CONFLICT DO NOTHING";
-        self.client.simple_query(query)?;
+        self.cluster_client.simple_query(query)?;
 
         let query = "ALTER SCHEMA neon_migration OWNER TO cloud_admin";
-        self.client.simple_query(query)?;
+        self.cluster_client.simple_query(query)?;
 
         let query = "REVOKE ALL ON SCHEMA neon_migration FROM PUBLIC";
-        self.client.simple_query(query)?;
+        self.cluster_client.simple_query(query)?;
 
         Ok(())
+    }
+
+    fn run_migration_with_client(
+        client: &mut Client,
+        migration_id: usize,
+        migration: &str,
+    ) -> Result<()> {
+        if migration.starts_with("-- SKIP") {
+            info!("Skipping migration id={}", migration_id);
+        } else {
+            info!("Running migration id={}:\n{}\n", migration_id, migration);
+            client
+                .simple_query(migration)
+                .with_context(|| format!("run_migration current_migration={}", migration_id))?;
+        }
+
+        Ok(())
+    }
+
+    fn run_migration(&self, db: &str, migration_id: usize, migration: &str) -> Result<()> {
+        let mut connstr = self.connstr.clone();
+        connstr.set_path(db);
+        connstr
+            .query_pairs_mut()
+            .append_pair("application_name", "migrations");
+
+        let mut client = Client::connect(connstr.as_str(), NoTls)?;
+
+        Self::run_migration_with_client(&mut client, migration_id, migration)
     }
 
     pub fn run_migrations(mut self) -> Result<()> {
@@ -60,24 +119,73 @@ impl<'m> MigrationRunner<'m> {
         let mut current_migration: usize = self.get_migration_id()? as usize;
         let starting_migration_id = current_migration;
 
-        let query = "BEGIN";
-        self.client
-            .simple_query(query)
+        let mut dbs: Option<HashMap<String, Database>> = None;
+        if self
+            .migrations
+            .iter()
+            .any(|m| matches!(m, Migration::PerDatabase(_)))
+        {
+            dbs = Some(get_existing_dbs(&mut self.cluster_client)?);
+        }
+
+        // A Postgres connection string will always have a path with 1 segment, the database name
+        let admin_db = self.connstr.path_segments().unwrap().next().unwrap();
+
+        self.cluster_client
+            .simple_query("BEGIN")
             .context("run_migrations begin")?;
 
         while current_migration < self.migrations.len() {
-            let migration = self.migrations[current_migration];
+            match &self.migrations[current_migration] {
+                Migration::Cluster(migration) => Self::run_migration_with_client(
+                    &mut self.cluster_client,
+                    current_migration,
+                    migration,
+                )?,
+                Migration::PerDatabase(migration) => {
+                    // Iterate over all non-invalid databases (datconnectivity = -2)
+                    for db in dbs.as_ref().unwrap().iter().filter(|d| !d.1.invalid) {
+                        /* Once all the databases have ran the migration, then we can run it in the
+                         * admin database to mark the migration as complete. See the run for the
+                         * admin database outside this loop.
+                         */
+                        if db.0 == admin_db {
+                            continue;
+                        }
 
-            if migration.starts_with("-- SKIP") {
-                info!("Skipping migration id={}", current_migration);
-            } else {
-                info!(
-                    "Running migration id={}:\n{}\n",
-                    current_migration, migration
-                );
-                self.client.simple_query(migration).with_context(|| {
-                    format!("run_migration current_migration={}", current_migration)
-                })?;
+                        info!("db: {} {}", db.0, db.1.restrict_conn);
+                        if db.1.restrict_conn {
+                            self.cluster_client.simple_query(
+                                format!("ALTER DATABASE \"{}\" WITH allow_connections true", db.0)
+                                    .as_str(),
+                            )?;
+                        }
+
+                        /* Do not early return, so that we can try to reset the connectability of
+                         * the db.
+                         */
+                        let result = self.run_migration(db.0, current_migration, migration);
+
+                        if db.1.restrict_conn {
+                            self.cluster_client.simple_query(
+                                format!("ALTER DATABASE \"{}\" WITH allow_connections false", db.0)
+                                    .as_str(),
+                            )?;
+                        }
+
+                        if result.is_err() {
+                            let _ = self.cluster_client.simple_query("ABORT");
+                            return result;
+                        }
+                    }
+
+                    // We can reuse the client here instead of creating a new one
+                    Self::run_migration_with_client(
+                        &mut self.cluster_client,
+                        current_migration,
+                        migration,
+                    )?;
+                }
             }
 
             current_migration += 1;
@@ -85,9 +193,8 @@ impl<'m> MigrationRunner<'m> {
 
         self.update_migration_id()?;
 
-        let query = "COMMIT";
-        self.client
-            .simple_query(query)
+        self.cluster_client
+            .simple_query("COMMIT")
             .context("run_migrations commit")?;
 
         info!(
